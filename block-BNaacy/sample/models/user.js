11d43f1b38b30e7cf51faa9a648ceb8512545019
farhaneas11 <farhan.salam11@gmail.com>
2021-07-23 16:10:45 +0530

    CRUD-express-2

---
+++ block-BNaacy/sample/models/user.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    name :{type : String , required :true},
+    age : Number
+},{timestamps:true});
+
+var User = mongoose.model('User',userSchema);
+
+module.exports = User;
